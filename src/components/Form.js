cb76fe0bd81cb2381cf610978302be649d800e80
ashwani <ashg821@gmail.com>
2022-05-16 10:37:43 +0530

    few changes the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -33,6 +33,11 @@ export default function Form(props) {
         setText(text);
         props.showAlert("success", "Spaces have been removed.");
     }
+    let copyText=()=>{
+        navigator.clipboard.writeText(text);
+        // alert("Text Copied!");
+        props.showAlert("success", "Text has been copied!");
+        }
   return (
     <>
     <div className="container my-4">
@@ -44,6 +49,7 @@ export default function Form(props) {
       <button className={`btn btn-primary mx-2 my-2 ${text.split(" ").filter((element)=>element.length!==0).join(" ").length===0?'disabled':""}`} onClick={onclickButtonHandlerLower}>Convert to Lowecase</button>
       <button className={`btn btn-primary mx-2 my-2 ${text.split(" ").filter((element)=>element.length!==0).join(" ").length===0?'disabled':""}`} onClick={insertComma}>Replace Space</button>
       <button className={`btn btn-primary mx-2 my-2 ${text.split(" ").filter((element)=>element.length!==0).join(" ").length===0?'disabled':""}`} onClick={removeSpaces}>Remove Spaces</button>
+      <button className={`btn btn-primary mx-2 my-2 ${text.split(" ").filter((element)=>element.length!==0).join(" ").length===0?'disabled':""}`} onClick={copyText}>Copy Text</button>
     </div>
     <div className="container my-3">
         <h2>Some other details</h2>
@@ -52,10 +58,10 @@ export default function Form(props) {
                 Number of character: {text.split(" ").filter((element)=>element.length!==0).join(" ").length}
             </li>
             <li>
-                Number of words: {text.split(" ").filter((element)=>element.length!==0).length}
+                Number of words: {text.split(/\s/g).filter((element)=>element.length!==0).length}
             </li>
             <li>    
-                {console.log(text.split(" ").filter((element)=>element.length!==0))}
+                {/* {console.log(text.split(" ").filter((element)=>element.length!==0))} */}
                 Estimated time to read(at speed 300WPM): {text.split(" ").filter((element)=>element.length!==0).length*0.0034} Minutes
             </li>
         </ul>
